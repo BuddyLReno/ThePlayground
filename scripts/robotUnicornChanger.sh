111b0c9c9f693bcb9ffd53d96a456825e4f597e6
Buddy Reno <buddy.reno@fm-renob.peace.daveramsey.com>
2013-05-29 17:01:41 -0500

    Removed extra echo command.

--- scripts/robotUnicornChanger.sh
+++ scripts/robotUnicornChanger.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-echo "You should lock your computer."
 echo "You should not blindly run scripts from the internet."
 osascript -e 'tell application "Finder" to activate' -e 'tell application "System Events"' -e 'tell application process "Finder"' -e 'tell menu bar 1' -e 'click menu item "Hide Others" of menu of menu bar item "Finder"' -e 'click menu item "Minimize All" of menu of menu bar item "Window"' -e 'end tell' -e 'end tell' -e 'end tell'
 cd ~/Desktop
