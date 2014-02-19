ddd2cfa72d3be83159e9688209d6177c5780957f
Buddy Reno <thebudman84@gmail.com>
2014-02-19 12:05:49 -0600

    Change robot unicorn image to Gawain.

--- scripts/robotUnicornChanger.sh
+++ scripts/robotUnicornChanger.sh
@@ -3,7 +3,10 @@
 echo "You should not blindly run scripts from the internet."
 osascript -e 'tell application "Finder" to activate' -e 'tell application "System Events"' -e 'tell application process "Finder"' -e 'tell menu bar 1' -e 'click menu item "Hide Others" of menu of menu bar item "Finder"' -e 'click menu item "Minimize All" of menu of menu bar item "Window"' -e 'end tell' -e 'end tell' -e 'end tell'
 cd ~/Desktop
-curl -o youshouldlockyourcomputer.png http://i.imgur.com/JMflWrl.jpg
+# Robot Unicorn
+# curl -o youshouldlockyourcomputer.png http://i.imgur.com/JMflWrl.jpg
+# Gawain
+curl -o youshouldlockyourcomputer.png http://i.imgur.com/h5uCDt8.jpg
 defaults write com.apple.desktop Background '{default = {ImageFilePath = "~/Desktop/youshouldlockyourcomputer.png"; }; }'
 killall Dock
 cd ~
