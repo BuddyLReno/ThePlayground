fb88ea9a775c933b9fc6a22715894d393e984cfc
buddylreno <buddy.reno@daveramsey.com>
2014-05-28 07:29:04 -0500

    change image back to robot unicorn.

--- scripts/robotUnicornChanger.sh
+++ scripts/robotUnicornChanger.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-echo "You should not blindly run scripts from the internet."
+echo "ALL HAIL ZEE ROBOT UNICORN"
 osascript -e 'tell application "Finder" to activate' -e 'tell application "System Events"' -e 'tell application process "Finder"' -e 'tell menu bar 1' -e 'click menu item "Hide Others" of menu of menu bar item "Finder"' -e 'click menu item "Minimize All" of menu of menu bar item "Window"' -e 'end tell' -e 'end tell' -e 'end tell'
 cd ~/Desktop
 # Robot Unicorn
-# curl -o youshouldlockyourcomputer.png http://i.imgur.com/JMflWrl.jpg
+ curl -o youshouldlockyourcomputer.png http://i.imgur.com/JMflWrl.jpg
 # Gawain
-curl -o youshouldlockyourcomputer.png http://i.imgur.com/h5uCDt8.jpg
+# curl -o youshouldlockyourcomputer.png http://i.imgur.com/h5uCDt8.jpg
 defaults write com.apple.desktop Background '{default = {ImageFilePath = "~/Desktop/youshouldlockyourcomputer.png"; }; }'
 killall Dock
